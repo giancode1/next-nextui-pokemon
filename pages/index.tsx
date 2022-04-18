671ad2ebd4feba165758034d7a97a552c84bf45e
giancode1 <gieg5c@gmail.com>
2022-04-18 01:33:59 -0500

    GetStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { Button, Text, theme, CssBaseline } from '@nextui-org/react';
 import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = (props) => {
+  console.log(props); //si se muestra en la consola de la pagina
   return (
     <Layout title={'Listado de Pokémons'}>
       <h1>Hola</h1>
@@ -47,11 +48,21 @@ const HomePage: NextPage = (props) => {
   )
 }
 
+// snipet:
+//nextgetStaticProps
 
-//de lado del servidor, no llega alc lietne a excepcion de los props
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+//se ejecuta solo de lado del servidor,solo se ejecuta en build-time, no llega al cliente a excepcion de los props
+//GetStaticProps solo usar en pages, por ejemplo no en componentes
+//en desarrollo se ejecuta cada vez que se renderiza la pagina, en build se ejecuta solo una vez
 export const getStaticProps: GetStaticProps = async (ctx) => { //solo se ejecuta en build-time
   // const { data } = await  // your fetch function here 
-  console.log("Hola mundo")
+  console.log("Hola mundo")  //en la consola
   return {
     props: {
       name: 'Giancarlo',
