054340a79ff0f14e6b1b1f2c3c9b663624fc68d1
giancode1 <gieg5c@gmail.com>
2022-04-18 22:48:23 -0500

    interface de Pokemon, generando los 151 paginas de pokemons

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,21 +1,19 @@
 //rafce  exportacion por defecto
 import { GetStaticProps, NextPage, GetStaticPaths } from "next";
-import { useRouter } from "next/router";
 import { Layout } from "../../components/layouts";
+import pokeApi from '../../api/pokeApi';
+import { Pokemon } from "../../interfaces";
 
 interface Props {
-  //pokemon: any
-  id: string;
-  name: string;
+  pokemon: Pokemon
 }
 
-const PokemonPage: NextPage<Props> = ({ id, name }) => {
-  const router = useRouter();
-  console.log(router.query);
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+//console.log(pokemon); 
   return (
     <Layout title="algun pokemon">
-      <h1>{router.query.id}</h1>
-      <h2>{id}-{name}</h2>
+      <h1>{pokemon.name}</h1>
+
     </Layout>
   );
 };
@@ -24,34 +22,31 @@ const PokemonPage: NextPage<Props> = ({ id, name }) => {
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
 //si no tuviera el path de forma dinamica: [id] , no deberia hacer esto
 //se ejecuta del lado del servidor, y solo en build time
+//tambn se ejecuta en desarrollo cuando se recarga la pag
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
+  //console.log("pokemons151:", pokemons151);
   return {
-      //la cantidad sera la cantidad de paginas que se van a generar
-    paths: [
-      {
-        params: { id: "1" }, //estrictamente debe ser un string
-      },
-      {
-        params: { id: "2" },
-      },
-      {
-        params: { id: "3" },
-      },
-    ],
-    // fallback: "blocking", // deja pasar
-    fallback: false,  //no deja pasar, sino existe el id definido arriba
+    paths: pokemons151.map((id) => ({ params: { id } })), // paths: [{ params: { id: '1' } }, { params: { id: '2' } }, ...]
+    fallback: false, //no deja pasar, sino existe el id definido arriba
   };
 };
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  //const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
-
+//primero se ejecuta getStaticPaths y despues getStaticProps
+// export const getStaticProps: GetStaticProps = async (ctx) => {
+  //console.log("ctx.params:", ctx.params); //el parametro de la url
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params as { id: string };
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
   return {
     props: {
-      id: 1,
-      name: "pikachu",
+      pokemon: data
     },
   };
 };
 
 export default PokemonPage;
+
+//generar los 151 pokemons
+//yarn build
+//yarn start
